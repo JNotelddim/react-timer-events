e4d6540337dfd126ef74228e7d2daad294e39bba
JNotelddim <JNotelddim@users.noreply.github.com>
2022-12-17 19:10:59 -0800

    Basic end event working

--- src/components/provider/ToastProvider.tsx
+++ src/components/provider/ToastProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import { SubComponentVariant } from "../SubComponent";
 import { Toast } from "../Toast";
@@ -30,17 +30,51 @@ export const useToast = () => {
   return useContext(ToastContext);
 };
 
-let counter = 1;
-
 export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
   const [toasts, setToasts] = useState<ToastProps[]>([]);
   //   const { startTimer /*pauseTimer, clearTimer*/ } = useTimers();
+  const [timerEvents, setTimerEvents] = useState<
+    CustomEventInit<{ id: string }>[]
+  >([]);
 
-  console.log({ toasts });
+  console.log({ toasts, timerEvents });
+
+  useEffect(() => {
+    if (timerEvents && timerEvents.length) {
+      console.log("useEffect from state event list updating");
+      const newEvent = timerEvents[timerEvents.length - 1];
+      const toastId = newEvent.detail?.id;
+      console.log({ newEvent, toastId });
+      setToasts([...toasts.filter((t) => t.id !== toastId)]);
+      setTimerEvents([
+        ...timerEvents.filter((t) => t?.detail?.id !== newEvent?.detail?.id),
+      ]);
+    }
+  }, [timerEvents, toasts]);
 
   const addToast = (newToast: ToastInput) => {
     const newToastId = nanoid();
     setToasts([...toasts, { ...newToast, id: newToastId }]);
+
+    const timerEndEventName = `timer-${newToastId}-end`;
+    document.addEventListener(
+      timerEndEventName,
+      (endEvent: CustomEventInit<{ id: string }>) => {
+        console.log("end event subscriber", endEvent);
+        // How to push to state array without using a ref to get most up-to-date state??
+        // I think it's impossible...
+        setTimerEvents([...timerEvents, endEvent]);
+      }
+    );
+
+    console.log("starting timeout", newToastId);
+    setTimeout(() => {
+      console.log("timeout ended, publishing event", timerEndEventName);
+      const endEvent = new CustomEvent(timerEndEventName, {
+        detail: { id: newToastId },
+      });
+      document.dispatchEvent(endEvent);
+    }, 5000);
 
     // set up event for "toast-{id}-timer-end"
     // setTimeout, () => { publishEvent(^) }, <duration>
