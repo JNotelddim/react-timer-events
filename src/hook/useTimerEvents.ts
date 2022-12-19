f26e60b29f73acb68f307b36c5e8be58627e60c7
JNotelddim <JNotelddim@users.noreply.github.com>
2022-12-19 10:45:35 -0800

    Swap timers data type from map to array (stack)

--- src/hook/useTimerEvents.ts
+++ src/hook/useTimerEvents.ts
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 
 export interface Timer {
-  timeout: NodeJS.Timeout;
-  isPaused?: boolean;
+  id: string;
   remaining: number;
   startTime: number;
+  timeout: NodeJS.Timeout;
+  isPaused?: boolean;
 }
 
 const DEFAULT_TIMEOUT = 5000;
@@ -22,56 +23,70 @@ export const useStatefulTimeouts = ({
   onTimerEnd: (id: string) => void;
   onTimerPause?: (id: string) => void;
 }) => {
-  const [timers, setTimers] = useState<Record<string, Timer>>({});
-  const [timerEvents, setTimerEvents] = useState<{ id: string }[]>([]);
-
-  //   console.log(
-  //     `timers: [${Object.keys(timers).join(", ")}]; events: [${timerEvents.join(
-  //       ", "
-  //     )}]`
-  //   );
+  const [timers, setTimers] = useState<Timer[]>([]);
+  const [timerEvents, setTimerEvents] = useState<
+    { id: string; type: "end" | "pause" }[]
+  >([]);
 
   useEffect(() => {
     // TODO: check that the event was an 'end' event?
     if (timerEvents && timerEvents.length) {
       const newEvent = timerEvents[timerEvents.length - 1];
-      onTimerEnd(newEvent.id);
+
+      if (newEvent.type === "end") {
+        onTimerEnd(newEvent.id);
+        // TODO: resume -- but this should be part of ^ callback , but it can't be because that'd be circular...
+        resumeTop();
+      }
 
       // Clear out this event so it's not retriggered
       setTimerEvents([...timerEvents.filter((t) => t?.id !== newEvent?.id)]);
 
       // Clear out this timer so it's not tracked anymore
-      const tempTimers = { ...timers };
-      delete tempTimers[newEvent.id];
+      const tempTimers = [...timers.filter((t) => t.id !== newEvent.id)];
       setTimers(tempTimers);
     }
   }, [timerEvents, onTimerEnd, timers]);
 
   const startTimer = (id: string, duration?: number) => {
-    setTimers({
+    setTimers([
       ...timers,
-      [id]: {
+      {
+        id,
         isPaused: true,
         remaining: duration || DEFAULT_TIMEOUT,
         startTime: Date.now(),
         timeout: setTimeout(() => {
-          setTimerEvents([...timerEvents, { id }]);
+          setTimerEvents([...timerEvents, { id, type: "end" }]);
         }, duration || DEFAULT_TIMEOUT),
       },
-    });
+    ]);
   };
 
-  const pauseTimer = (id: string) => {
-    if (!Object.keys(timers).includes(id)) {
+  const resumeTop = () => {
+    if (timers.length <= 0) {
       return;
     }
 
-    const timer = { ...timers[id] };
-    timer.isPaused = true;
-    timer.remaining -= Date.now() - timer.startTime;
+    console.log("todo, resume top");
+
+    // TODO: resume top timer
+    // const newTop =
+  };
+
+  const pauseTimer = (id: string) => {
+    const timer = timers.find((t) => t.id);
+    if (!timer) {
+      return;
+    }
+
+    const tmpTimer = { ...timer };
+    tmpTimer.isPaused = true;
+    tmpTimer.remaining -= Date.now() - timer.startTime;
     clearTimeout(timer.timeout);
 
-    onTimerPause?.(id);
+    // TODO: read onPause later
+    // onTimerPause?.(id);
     setTimers({ ...timers, [id]: timer });
   };
 
